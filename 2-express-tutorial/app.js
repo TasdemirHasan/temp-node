b7f174cf1f6d8966bfa31573e0c5229488fa6ffc
Tirej <hhtasdemir92@gmail.com>
2023-01-14 23:55:47 -0500

    hello

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -7,17 +7,17 @@ const auth = require('./routes/auth')
 //static assets
 app.use(express.static('./methods-public'))
 //parse form data
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 //parse json
 app.use(express.json())
 
 
-app.use('/api/people',people)
-app.use('/login',auth)
+app.use('/api/people', people)
+app.use('/login', auth)
 
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000....')
 })
 
-//orange ali git git
+//orange ali git git.a
